5a3bc9bc74447611398baaffd42d0843020044cb
kaua0910 <kaua.alameida0910@gmail.com>
2026-05-05 08:38:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var n1 = document.querySelector('#n1');
+var n2 = document.querySelector('#n2');
+var resultado = document.querySelector('#resultado');
+
+function somar() {
+    resultado.innerHTML = Number(n1.value) + Number(n2.value);
+}
+
+function subtrair() {
+    resultado.innerHTML = Number(n1.value) - Number(n2.value);
+}
+
+function multiplicar() {
+    resultado.innerHTML = Number(n1.value) * Number(n2.value);
+}
+
+function dividir() {
+    if (Number(n2.value) === 0) {
+        resultado.innerHTML = 'Erro: Divisão por zero';
+    } else {
+        resultado.innerHTML = Number(n1.value) / Number(n2.value);
+    }
+}
